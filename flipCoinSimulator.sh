1b30e677b2fb72f9780051f9ef2c4efbb6e0afe4
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-08 12:06:16 +0530

    ADDED : percentage of singlet

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,43 @@
 #!/bin/bash -x
-echo "WelCome to Flip Coin Simulation"
 
-coinFlip=$((RANDOM%2))
-if [ $coinFlip -eq 1 ]
-then
-	echo "Head"
-else
-	echo "Tail"
+declare -A singletDict
+
+function singlet()
+	{
+		NoofFlip=$1
+		singletDict[H]=0
+      singletDict[T]=0
+		coin=1
+		for (( i=0;i<$NoofFlip;i++ ))
+		do
+			for (( j=0;j<$coin;j++ ))
+			do
+	      	str=" "
+					CoinFlip=$((RANDOM%2))
+						if [ $CoinFlip -eq 1 ]
+                  then
+                  	str="H"
+                  else
+                  	str="T"
+                  fi
+			done
+	singletDict[$str]=$((${singletDict[$str]}+1))
+	done
+	
+	findPercentage
+	}
+ 
+function findPercentage()
+	{
+		for key in ${!singletDict[@]}
+		do
+			singletDict[$key]=$((${singletDict[$key]}*100/$NoofFlip))
+		done
+	}
+ 
+read -p "Do you Want to flip a coin (Y/N) :" option
+if [[ $option == "Y" || $option == "y" ]]
+then 
+	read -p "How many Times you want flip a coin : " NoofTimes
+	singlet $NoofTimes
 fi
-
