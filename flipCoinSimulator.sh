5090df7febb1f876ac605f95cfcc90fc8080fb8f
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-08 12:12:31 +0530

    ADDED : percentage of singlet

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -6,21 +6,21 @@ function singlet()
 	{
 		NoofFlip=$1
 		singletDict[H]=0
-      singletDict[T]=0
-		coin=1
+		singletDict[T]=0
+		NoofCoin=1
 		for (( i=0;i<$NoofFlip;i++ ))
 		do
-			for (( j=0;j<$coin;j++ ))
+			for (( j=0;j<$NoofCoin;j++ ))
 			do
-	      	str=" "
-					CoinFlip=$((RANDOM%2))
-						if [ $CoinFlip -eq 1 ]
-                  then
-                  	str="H"
-                  else
-                  	str="T"
-                  fi
-			done
+				str=" "
+				CoinFlip=$((RANDOM%2))
+				if [ $CoinFlip -eq 1 ]
+				then
+					str="H"
+				else
+					str="T"
+				fi
+		done
 	singletDict[$str]=$((${singletDict[$str]}+1))
 	done
 	
